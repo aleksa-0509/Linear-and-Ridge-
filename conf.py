4462dda8b32bec53f9de0176c891de45f38d67dc
aleksa-0509 <116683113+aleksa-0509@users.noreply.github.com>
2022-12-20 15:48:27 +0300

    Add files via upload

--- conf.py
+++ conf.py
@@ -1,14 +1,4 @@
-Original file is located at
-    https://colab.research.google.com/drive/1CiinNpAk6ydLkZbQIFJ30U-zcFopjAnh
+from conf.conf import logging
+import logging 
 
-pip install dynaconf
-
-import logging
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=['conf\\settings.toml'],
-)
-
-logging.basicConfig(level=settings.LOG_LEVEL, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO)
---
+++ connector.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def extract_data(link: str) -> pd.DataFrame
+
+        df = pd.read_csv('https://raw.githubusercontent.com/5x12/ml-cookbook/master/supplements/data/cars.csv')
+
+return df 
