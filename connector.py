a37e122a54ac861cbd9263dccc711da8ac28dc8c
aleksa-0509 <116683113+aleksa-0509@users.noreply.github.com>
2022-12-22 18:52:13 +0300

    Delete connector.py

--- connector.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def extract_data(link: str) -> pd.DataFrame
-
-        df = pd.read_csv('https://raw.githubusercontent.com/5x12/ml-cookbook/master/supplements/data/cars.csv')
-
-return df 
